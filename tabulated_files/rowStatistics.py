e7d323124971da472968f5a2889be1bf3e5095d2
ajendrex <hurbinas@gmail.com>
2012-10-10 21:15:22 -0300

    improving output

--- tabulated_files/rowStatistics.py
+++ tabulated_files/rowStatistics.py
@@ -13,6 +13,7 @@ def main():
   commandLineParser = argparse.ArgumentParser(description="Prints row statistics from a (set of) tabulated input file(s)")
   commandLineParser.add_argument(metavar='FILE', nargs='+', dest="inputFiles", help='Tabulated input files with numbers in columns to be considered (See -c flag)')
   commandLineParser.add_argument('-c', '--column', dest="columns", nargs='+', default='all', help='columns to be considered, starting from 1. default: all columns.')
+  commandLineParser.add_argument('-d', '--detailed', dest="detailed", default=False, action='store_true', help='Prints columns before statistics.')
   options = commandLineParser.parse_args()
 
   files = []
@@ -24,19 +25,19 @@ def main():
     except IOError:
       print("Couldn't open " + filename)
       sys.exit()
-  
+
   for f in files:
     fileReaders.append(csv.reader(f, delimiter='\t'))
 
   rowNumber = 1
+  printHeaders = True
   for row in fileReaders[0]:
     nums = []
     try:
       if options.columns == "all":
-        nums = [float(n) for n in row]
-      else:
-        for column in options.columns:
-          nums.append(float(row[int(column)-1]))
+        options.columns = [x+1 for x in range(len(row))]
+      for column in options.columns:
+        nums.append(float(row[int(column)-1]))
     except ValueError:
       rowNumber += 1
       if len(fileReaders) > 1:
@@ -51,11 +52,8 @@ def main():
       if len(fileReaders) > 1:
         for reader in fileReaders[1:]:
           r = next(reader)
-          if options.columns == "all":
-            nums.extend([float(n) for n in r])
-          else:
-            for column in options.columns:
-              nums.append(float(r[int(column)-1]))
+          for column in options.columns:
+            nums.append(float(r[int(column)-1]))
     except ValueError:
       print("One of the files (not the first one given) has a non-numeric value where the first one does have it. Line " + str(rowNumber))
       sys.exit()
@@ -66,7 +64,20 @@ def main():
     npNums = numpy.array(nums)
     mean = npNums.mean()
     std = npNums.std()
-    print("row " + str(rowNumber) + ": mean = " + str(mean) + ", std = " + str(std))
+    if printHeaders:
+      columnNames = ["row"]
+      if options.detailed:
+        for f in options.inputFiles:
+          columnNames.extend(["col" + str(c) + "_" + f for c in options.columns])
+      columnNames.extend(["mean", "std"])
+      print("\t".join(columnNames))
+      printHeaders = False
+
+    print(str(rowNumber) + "\t", end="")
+    if options.detailed:
+      print("\t".join([str(n) for n in nums]), end="")
+      print("\t", end="")
+    print(str(mean) + "\t" + str(std))
     rowNumber += 1
 
 main()
