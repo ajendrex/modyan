c0fda4be391119f5e4b0d2996ffcaa83de2b79e9
ajendrex <hurbinas@gmail.com>
2013-06-20 23:50:49 -0400

    splitFastaSequences.py puede dividir secuencias con separadores compuestos por cualquier combinacion de varias letras

--- sequence/splitFastaSequences.py
+++ sequence/splitFastaSequences.py
@@ -13,7 +13,7 @@ def main():
   commandLineParser = argparse.ArgumentParser(description="Split squences from fasta according to a given separator.")
   commandLineParser.add_argument(metavar='fastaIN', dest="fasta", help='fasta input file.')
   commandLineParser.add_argument(metavar='fastaOUT', dest="out", help='fasta output file name.')
-  commandLineParser.add_argument('-s', '--separator', dest="sep", default='n', help='Separator to be used.')
+  commandLineParser.add_argument('-s', '--separator', dest="seps", default=['n'], nargs="+", help='Separators to be used.')
   options = commandLineParser.parse_args()
 
   inputHandle = open(options.fasta, "rU")
@@ -21,12 +21,13 @@ def main():
   for record in SeqIO.parse(inputHandle, "fasta"):
     i = 1
     seqName = record.id
-    for subseq in re.split(options.sep + '+', str(record.seq)):
+    for subseq in re.split('[' + '|'.join(options.seps) + ']+', str(record.seq)):
       myRecord = SeqRecord.SeqRecord(Seq.Seq(subseq))
       myRecord.id = seqName + "_part" + str(i)
       myRecord.description = ""
       SeqIO.write(myRecord, outputHandle, "fasta")
       sys.stdout.flush()
+      i += 1
   
   inputHandle.close()
   outputHandle.close()
